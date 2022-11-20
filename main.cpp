51ce9aa2bf793836715e5611e664463edc161ebb
OrrBav <orrbavly@gmail.com>
2022-11-20 22:21:09 +0200

    adding input check - vector size comparison

--- main.cpp
+++ main.cpp
@@ -66,14 +66,26 @@ vector <float> check(string input_str, vector <float>& numbers) {
 
 int main()
 {
-    string str;
-    vector<float> numbers;
-    getline(cin, str);
+    string str1, str2;
+    vector<float> numbers1, numbers2;
+    getline(cin, str1);
     try {
-        check(str, numbers);
+        check(str1, numbers1);
     }
     catch (invalid_argument& e) {
         cout << e.what() << endl;
+        return -1;
+    }
+    getline(cin, str2);
+    try {
+        check(str2, numbers2);
+    }
+    catch (invalid_argument& e) {
+        cout << e.what() << endl;
+        return -1;
+    }
+    if (numbers1.size() != numbers2.size()) {
+        cout << "inputs should have the same number of numbers";
         return -1;
     }
     cout << "success";
