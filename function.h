dfe10d338c893fa9865f882691bcf44380b6f09f
Ariel Ashkenazy <ariel.ashkenazy@gmail.com>
2022-11-22 16:09:45 +0200

    updated header

--- function.h
+++ function.h
@@ -1,10 +1,23 @@
-//
-// Created by orrbb on 21/11/2022.
-//
+
 #include <vector>
 #include <string>
 using namespace std;
+
 #ifndef ADVANCED_PROGRAMMING_1_FUNCTION_H
 #define ADVANCED_PROGRAMMING_1_FUNCTION_H
+
 vector <float> check(string input_str, vector <float>& numbers);
+
+// helper Functions:
+vector <float> vectorSubstruction(vector <float> v1, vector <float> v2);
+vector <float> vectorRaiseToPower(vector <float> vector, int toThePowerOf = 2);
+vector <float> vectorSquareRoot(vector <float> vector);
+vector <float> vectorAbsolute(vector <float> vector);
+float vectorSum(vector <float> vector);
+vector <float> vectorAddition(vector <float> v1, vector <float> v2);
+
+// distance functions:
+float euclideanDistance(vector <float> v1, vector <float> v2);
+
+
 #endif //ADVANCED_PROGRAMMING_1_FUNCTION_H
--- vectorFuncs.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-
-vector <float> vectorSubstruction(vector <float> v1, vector <float> v2);
-
