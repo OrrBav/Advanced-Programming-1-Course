99fce54e0d348db6234d8d7cc366a53de777e8e8
OrrBav <orrbavly@gmail.com>
2022-12-14 16:15:13 +0200

    pushed main.h

---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by orrbb on 14/12/2022.
+//
+
+#ifndef ADVANCED_PROGRAMMING_1_MAIN_H
+#define ADVANCED_PROGRAMMING_1_MAIN_H
+#define ARG_SIZE (4)
+#include <iostream>
+#include <string>
+#include "knn.h"
+#include "readFromFile.h"
+
+#endif //ADVANCED_PROGRAMMING_1_MAIN_H
