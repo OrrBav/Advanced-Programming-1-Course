5648a0be09bf5eea8ba2475b7e42ba901448255f
OrrBav <orrbavly@gmail.com>
2022-12-24 15:50:15 +0200

    added libraries

--- server.cpp
+++ server.cpp
@@ -1,7 +1,15 @@
 //
 // Created by orrbb on 24/12/2022.
 //
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
+using namespace std;
 int main () {
 
 }
